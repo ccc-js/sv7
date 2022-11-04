d6ac2dfc2e8c7e5857de99217a35ae2ca0a52ed7
ccckmit <ccckmit@gmail.com>
2022-11-04 11:07:02 +0800

    xv6=>xv7

--- test/sv6_test.js
+++ test/sv6_test.js
@@ -5,7 +5,9 @@ const server = new sv6.Server()
 server.router.get("/", (ctx) => {
   ctx.response.body = "Hello sv6!";
 })
+
 server.route()
+
 Deno.test("it should support the Oak framework", async () => {
   const request = await superoak(server.app);
   await request.get("/").expect("Hello sv6!");
